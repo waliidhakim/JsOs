a76cf6d1ff9d02cac0f125cccfe261a3113dc01f
Walid Khalqallah <khalqallahwalid@gmail.com>
2023-02-24 23:39:52 +0100

    interface progress

--- main.js
+++ main.js
@@ -1,49 +1,17 @@
-import Root from './components/root.js';
-import Calculator from './components/calculator.js';
-import Clock from './components/clock.js';
-import Settings from './components/settings.js';
-import TicTacToe from './components/TicTacToe.js';
+import { App } from './components/app.js'
 
-console.log('hello world')
-const root = new Root({
-    components: [
-        new Calculator(),
-        new Clock(),
-        new TicTacToe(),
-        new Settings()
-    ]
+document.addEventListener('DOMContentLoaded', () => {
+    const calculatorApp = document.querySelector('.calculator');
+    new App(calculatorApp, 'calculator');
+    
+    const clockApp = document.querySelector('.clock');
+    new App(clockApp, 'clock');
+
+    const ticTacToeApp = document.querySelector('.tictactoe');
+    new App(ticTacToeApp, 'tic-tac-toe');
+
+    const settingsApp = document.querySelector('.settings');
+    new App(settingsApp, 'settings');
+
 });
 
-const appsContainer = document.getElementById('app');
-
-const calculatorButton = document.getElementById('calculator-button');
-const clockButton = document.getElementById('clock-button');
-const ticTacToeButton = document.getElementById('tic-tac-toe-button');
-const settingsButton = document.getElementById('settings-button');
-
-calculatorButton.addEventListener('click', () => {
-    const calculator = new Calculator();
-    appsContainer.innerHTML = '';
-    appsContainer.appendChild(calculator.element);
-    console.log('calculator clicked')
-});
-
-clockButton.addEventListener('click', () => {
-    const clock = new Clock();
-    appsContainer.innerHTML = '';
-    appsContainer.appendChild(clock.element);
-});
-
-ticTacToeButton.addEventListener('click', () => {
-    const ticTacToe = new TicTacToe();
-    appsContainer.innerHTML = '';
-    appsContainer.appendChild(ticTacToe.element);
-});
-
-settingsButton.addEventListener('click', () => {
-    const settings = new Settings();
-    appsContainer.innerHTML = '';
-    appsContainer.appendChild(settings.element);
-});
-
-root.render();
