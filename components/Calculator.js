febee42523d02f7537a47a89d3fa408ad995c781
zakaria-perso <aidouni.zak@gmail.com>
2023-02-26 11:37:35 +0100

    add more features

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,128 +1,137 @@
-// Select all the elements from the DOM
-const outputPreviousOperand = document.querySelector('[data-previous-operand]');
-const outputCurrentOperand = document.querySelector('[data-current-operand]');
-const buttonsAllClear = document.querySelector('[data-all-clear]');
-const buttonsDelete = document.querySelector('[data-delete]');
-const buttonsNumbers = document.querySelectorAll('[data-number]');
-const buttonsOperations = document.querySelectorAll('[data-operation]');
-const buttonsEquals = document.querySelector('[data-equals]');
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
 
-// Initialize the calculator
-let currentOperand = '';
-let previousOperand = '';
-let operation = undefined;
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+const lastOperationScreen = document.getElementById('lastOperationScreen')
+const currentOperationScreen = document.getElementById('currentOperationScreen')
 
-// Add an event listener to each number button
-buttonsNumbers.forEach(button => {
-  button.addEventListener('click', () => {
-    addNumber(button.textContent);
-    updateDisplay();
-  });
-});
+window.addEventListener('keydown', handleKeyboardInput)
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+pointButton.addEventListener('click', appendPoint)
 
-// Add an event listener to each operation button
-buttonsOperations.forEach(button => {
-  button.addEventListener('click', () => {
-    setOperation(button.textContent);
-    updateDisplay();
-  });
-});
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
 
-// Add an event listener to the equals button
-buttonsEquals.addEventListener('click', () => {
-  compute();
-  updateDisplay();
-  window.navigator.vibrate(200);
-});
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
 
-// Add an event listener to the all-clear button
-buttonsAllClear.addEventListener('click', () => {
-  clear();
-  updateDisplay();
-});
-
-// Add an event listener to the delete button
-buttonsDelete.addEventListener('click', () => {
-  deleteNumber();
-  updateDisplay();
-});
-
-// Add a number to the current operand
-export function addNumber(number) {
-  if (currentOperand === 'MathError') currentOperand = ''
-  if (number === '.' && currentOperand.includes('.')) return;
-  if (number === '-' && currentOperand === '') {
-    currentOperand = '-';
-    return;
-  }
-  if (number === '-' && currentOperand !== '' && previousOperand === '') {
-    currentOperand = '-' + currentOperand;
-    return;
-  }
-  currentOperand += number;
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === '0' || shouldResetScreen)
+    resetScreen()
+  currentOperationScreen.textContent += number
 }
 
-// Set the operation
-export function setOperation(op) {
-  if (currentOperand === 'MathError') currentOperand = ''
-  if (currentOperand === '' && op != '-') return;
-  if (currentOperand === '' && op == '-')
-    currentOperand = '-';
-  console.log(currentOperand)
-  if (currentOperand !== '-') {
-    compute();
-    operation = op;
-    previousOperand = currentOperand;
-    currentOperand = '';
-  }
+function resetScreen() {
+  currentOperationScreen.textContent = ''
+  shouldResetScreen = false
 }
 
-// Compute the result
-export function compute() {
-  let computation;
-  const prev = parseFloat(previousOperand);
-  const current = parseFloat(currentOperand);
-  if (isNaN(prev) || isNaN(current)) return;
-  switch (operation) {
+function clear() {
+  currentOperationScreen.textContent = '0'
+  lastOperationScreen.textContent = ''
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+
+function appendPoint() {
+  if (shouldResetScreen) resetScreen()
+  if (currentOperationScreen.textContent === '')
+    currentOperationScreen.textContent = '0'
+  if (currentOperationScreen.textContent.includes('.')) return
+  currentOperationScreen.textContent += '.'
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+      .toString()
+      .slice(0, -1)
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate()
+  firstOperand = currentOperationScreen.textContent
+  currentOperation = operator
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
+  shouldResetScreen = true
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  secondOperand = currentOperationScreen.textContent
+  currentOperationScreen.textContent = roundResult(
+      operate(currentOperation, firstOperand, secondOperand)
+  )
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+  currentOperation = null
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000
+}
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+  if (e.key === '.') appendPoint()
+  if (e.key === '=' || e.key === 'Enter') evaluate()
+  if (e.key === 'Backspace') deleteNumber()
+  if (e.key === 'Escape') clear()
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+    setOperation(convertOperator(e.key))
+}
+
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === '/') return '÷'
+  if (keyboardOperator === '*') return '×'
+  if (keyboardOperator === '-') return '−'
+  if (keyboardOperator === '+') return '+'
+}
+
+function add(a, b) {
+  return a + b
+}
+
+function substract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function operate(operator, a, b) {
+  a = Number(a)
+  b = Number(b)
+  switch (operator) {
     case '+':
-      computation = prev + current;
-      break;
-    case '-':
-      computation = prev - current;
-      break;
-    case '*':
-      computation = prev * current;
-      break;
-    case '/':
-      computation = prev / current;
-      break;
+      return add(a, b)
+    case '−':
+      return substract(a, b)
+    case '×':
+      return multiply(a, b)
+    case '÷':
+      if (b === 0) return null
+      else return divide(a, b)
     default:
-      return;
-  }
-  if (computation === Infinity) computation = 'MathError'
-  console.log(computation)
-  currentOperand = computation;
-  operation = undefined;
-  previousOperand = '';
-}
-
-// Clear the calculator
-export function clear() {
-  currentOperand = '';
-  previousOperand = '';
-  operation = undefined;
-}
-
-// Delete a number from the current operand
-export function deleteNumber() {
-  currentOperand = currentOperand.toString().slice(0, -1);
-}
-
-// Update the display
-export function updateDisplay() {
-  outputCurrentOperand.textContent = currentOperand;
-  outputPreviousOperand.textContent = previousOperand;
-  if (operation != null) {
-    outputPreviousOperand.textContent += ' ' + operation;
+      return null
   }
 }
--- components/Clock.js
+++ components/Clock.js
@@ -1,26 +1,360 @@
-function updateClock() {
-  var currentTime = new Date();
-  // Operating System Clock Hours for 12h clock
-  var currentHoursAP = currentTime.getHours();
-  // Operating System Clock Hours for 24h clock
-  var currentHours = currentTime.getHours();
-  // Operating System Clock Minutes
-  var currentMinutes = currentTime.getMinutes();
-  // Operating System Clock Seconds
-  var currentSeconds = currentTime.getSeconds();
-  // Adding 0 if Minutes & Seconds is More or Less than 10
-  currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-  currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
-  // Picking "AM" or "PM" 12h clock if time is more or less than 12
-  var timeOfDay = (currentHours < 12) ? "AM" : "PM";
-  // transform clock to 12h version if needed
-  currentHoursAP = (currentHours > 12) ? currentHours - 12 : currentHours;
-  // transform clock to 12h version after mid night
-  currentHoursAP = (currentHoursAP == 0) ? 12 : currentHoursAP;
-  // display first 24h clock and after line break 12h version
-  var currentTimeString = "24h kello: " + currentHours + ":" + currentMinutes + ":" + currentSeconds + "" + "<br>" + "12h kello: "    + currentHoursAP + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-  // print clock js in div #clock.
-  $(".clock-app").html(currentTimeString);}
-  $(document).ready(function () {
-  setInterval(updateClock, 1000);
+////////////////// chrono
+const watch = document.querySelector('#watch');
+let milliseconds = 0;
+let timer;
+
+function startWatch() {
+  watch.classList.remove('paused');
+  clearInterval(timer);
+  timer = setInterval(()=>{
+    milliseconds += 10;
+    let dateTimer = new Date(milliseconds);
+    watch.innerHTML =
+        ('0'+dateTimer.getUTCHours()).slice(-2) + ':' +
+        ('0'+dateTimer.getUTCMinutes()).slice(-2) + ':' +
+        ('0'+dateTimer.getUTCSeconds()).slice(-2) + ':' +
+        ('0'+dateTimer.getUTCMilliseconds()).slice(-3,-1);
+  },10);
+};
+
+function pauseWatch() {
+  watch.classList.add('paused');
+  clearInterval(timer);
+};
+
+function resetWatch() {
+  watch.classList.remove('paused');
+  clearInterval(timer);
+  milliseconds = 0;
+  watch.innerHTML= '00:00:00:00';
+};
+
+document.addEventListener('click', (e) =>{
+  const el = e.target;
+  if (el.id === 'start') startWatch();
+  if (el.id === 'pause') pauseWatch();
+  if (el.id === 'reset') resetWatch();
 });
+//////////////////
+
+/* grab necessary elements */
+// grab the .form
+const form = document.querySelector('.form');
+// grab the .time-input
+const timeInput = document.querySelector('.time-input');
+// grab the select[name='format']
+const format = document.querySelector("select[name='format']");
+// grab the .set-btn
+const setBtn = document.querySelector('.set-btn');
+// grab the .countdown
+const countDown = document.querySelector('.countdown');
+// grab the .stop-btn 
+const stopBtn = document.querySelector('.stop-btn');
+// grab the .reset-btn
+const resetBtn = document.querySelector('.reset-btn');
+/* grab necessary elements ends */ 
+
+
+/* global variables and constants*/
+// variable to store setInterval
+let countDownInterval;
+
+// secondsLeft in millisecond
+let secondsLeftms;
+// end time
+let endTime;
+// .stop-btn clicked or not
+let stopBtnClicked = false;
+/* global variables ends */
+
+
+/* .stop-btn click listener */
+stopBtn.addEventListener('click', () => {
+  // toggle the value of 'stopBtnClicked'
+  stopBtnClicked = !stopBtnClicked;
+
+  // if STOP button is clicked
+  if (stopBtnClicked === true) {
+    // change the text to 'PLAY'
+    stopBtn.innerHTML = 'PLAY';
+    // enable the .reset-btn
+    resetBtn.disabled = false;
+    // clear the setInterval() inorder to freeze the countdown timer
+    clearInterval(countDownInterval);
+  } else if (stopBtnClicked === false) {
+    // if PLAY button is clicked
+    // then change text to 'STOP'
+    stopBtn.innerHTML = 'STOP';
+    // disable the .reset-btn
+    resetBtn.disabled = true;
+    // then update endTime
+    endTime = secondsLeftms + Date.now();
+    // set a new setInterval()
+    countDownInterval = setInterval(() => {
+      setCountDown(endTime);
+    }, 1000);
+  }
+});
+/* .stop-btn click listener ends */
+
+
+/* .reset-btn click listener */
+resetBtn.addEventListener('click', () => {
+  resetCountDown();
+});
+/* .reset-btn click listener ends */
+
+
+/* .form submit listener */
+form.addEventListener('submit', (event) => {
+  // prevent the default page reloading
+  event.preventDefault();
+
+  // get the countdown time user typed
+  let countDownTime = timeInput.value;
+
+  // check if it is not zero
+  if (countDownTime > 0) {
+    // check which is the format, ie the <select> element's value
+    if (format.value === 'hours') {
+      // convert hours to milliseconds
+      // 1 hrs = 3600000 ms (5 zeros)
+      countDownTime = countDownTime * 3600000;
+    } else if (format.value === 'minutes') {
+      // 1 minute = 60000 ms (4 zeros)
+      countDownTime = countDownTime * 60000;
+    } else if (format.value === 'seconds') {
+      // 1 seconds = 1000 ms (3 zeros)
+      countDownTime = countDownTime * 1000;
+    }
+
+    // get current time in milliseconds
+    const now = Date.now();
+    // calculate the ending time
+    endTime = now + countDownTime;
+
+    // activate the countdown at first
+    setCountDown(endTime);
+
+    countDownInterval = setInterval(() => {
+      setCountDown(endTime);
+    }, 1000);
+
+    // then disable the .set-btn
+    setBtn.disabled = true;
+    // then enable the .stop-btn
+    stopBtn.disabled = false;
+  }
+
+});
+/* .form submit listener ends */
+
+
+/* setCountDown function */
+const setCountDown = (endTime) => {
+  // calculate how many milliseconds is left to reach endTime from now
+  secondsLeftms = endTime - Date.now();
+  // convert it to seconds
+  const secondsLeft = Math.round(secondsLeftms / 1000);
+
+  // calculate the hours, minutes and seconds
+  let hours = Math.floor(secondsLeft / 3600);
+  let minutes = Math.floor(secondsLeft / 60) - (hours * 60);
+  let seconds = secondsLeft % 60;
+
+  // adding an extra zero infront of digits if it is < 10
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  // stopping the timer if the time is up 
+  if (secondsLeft < 0) {
+    resetCountDown();
+    return;
+  }
+
+  // set the .countdown text
+  countDown.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+
+};
+/* setCountDown function ends */
+
+
+/* resetCountDown function */
+const resetCountDown = () => {
+  // destroy the setInterval()
+  clearInterval(countDownInterval);
+  // reset the countdown text
+  countDown.innerHTML = '00 : 00 : 00';
+  // set stopBtnClicked = false
+  stopBtnClicked = false;
+  // change inner text to STOP
+  stopBtn.innerHTML = 'STOP';
+
+  // enable .set-btn
+  setBtn.disabled = false;
+
+  // disable .stop-btn and .reset-btn
+  stopBtn.disabled = true;
+  resetBtn.disabled = true;
+};
+/* resetCountDown function ends */
+
+
+
+
+
+
+
+
+setInterval(function(){
+  getId("do-time").innerHTML = formatTime();
+},1000);
+
+function formatTime() {
+
+  var d = new Date(),
+      minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+      hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+      ampm = d.getHours() >= 12 ? 'pm' : 'am',
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  return '<h2>'+hours+'<span>:'+minutes+'</span></h2><small>'+ampm+'</small><h3>'+days[d.getDay()]+'<span>'+months[d.getMonth()]+' '+d.getDate()+'</span>'+d.getFullYear()+'</h3>';
+}
+
+var Cal = function(divId) {
+
+  this.divId = divId;
+
+  this.DaysOfWeek = [ 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa' ];
+
+  this.Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+
+  var d = new Date();
+
+  this.currMonth = d.getMonth();
+  this.currYear = d.getFullYear();
+  this.currDate = d.getDate();
+  this.currD = d.getDay();
+
+};
+
+Cal.prototype.nextMonth = function() {
+  if ( this.currMonth == 11 ) {
+    this.currMonth = 0;
+    this.currYear = this.currYear + 1;
+  }
+  else {
+    this.currMonth = this.currMonth + 1;
+  }
+  this.showcurr();
+};
+
+Cal.prototype.previousMonth = function() {
+  if ( this.currMonth == 0 ) {
+    this.currMonth = 11;
+    this.currYear = this.currYear - 1;
+  }
+  else {
+    this.currMonth = this.currMonth - 1;
+  }
+  this.showcurr();
+};
+
+Cal.prototype.showcurr = function() {
+  this.showMonth(this.currYear, this.currMonth);
+};
+
+Cal.prototype.showMonth = function(y, m) {
+
+  var chk = new Date();
+  var chkY = chk.getFullYear();
+  var chkM = chk.getMonth();
+
+  var d = new Date()
+  , firstDayOfMonth = new Date(y, m, 1).getDay()
+  , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
+  , lastDayOfLastMonth = m == 0 ? new Date(y-1, 11, 0).getDate() : new Date(y, m, 0).getDate();
+
+
+  var html = '<table>';
+
+  html += '<thead><tr>';
+  html += '<td colspan="7">' + this.Months[m] + ' ' + y + '</td>';
+  html += '</tr></thead>';
+
+
+  html += '<tr class="days">';
+  for(var i=0; i < this.DaysOfWeek.length;i++) {
+    if ( chkY == this.currYear && chkM == this.currMonth && i == this.currD ) {
+      html += '<td class="today">' + this.DaysOfWeek[i] + '</td>';
+    } else {
+      html += '<td>' + this.DaysOfWeek[i] + '</td>';
+    }
+  }
+  html += '</tr>';
+
+  var i=1;
+  do {
+
+    var dow = new Date(y, m, i).getDay();
+
+    if ( dow == 0 ) {
+      html += '<tr>';
+    }
+    else if ( i == 1 ) {
+      html += '<tr>';
+      var k = lastDayOfLastMonth - firstDayOfMonth+1;
+      for(var j=0; j < firstDayOfMonth; j++) {
+        html += '<td class="not-current">' + k + '</td>';
+        k++;
+      }
+    }
+
+    if (chkY == this.currYear && chkM == this.currMonth && i == this.currDate) {
+      html += '<td class="today">' + i + '</td>';
+    } else {
+      html += '<td class="normal">' + i + '</td>';
+    }
+    if ( dow == 6 ) {
+      html += '</tr>';
+    }
+    else if ( i == lastDateOfMonth ) {
+      var k=1;
+      for(dow; dow < 6; dow++) {
+        html += '<td class="not-current">' + k + '</td>';
+        k++;
+      }
+    }
+
+    i++;
+  }while(i <= lastDateOfMonth);
+
+  html += '</table>';
+
+  document.getElementById(this.divId).innerHTML = html;
+};
+
+window.onload = function() {
+
+  var c = new Cal("divCal");			
+  c.showcurr();
+
+  getId('btnNext').onclick = function() {
+    c.nextMonth();
+  };
+  getId('btnPrev').onclick = function() {
+    c.previousMonth();
+  };
+}
+
+function getId(id) {
+  return document.getElementById(id);
+}
